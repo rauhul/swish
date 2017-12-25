856695454a3b58f854ee665a152e06d572f588b6
Tanishq Dubey <tanishq.dubey@gmail.com>
2017-12-24 22:01:10 -0600

    Add builtin functionality

---
+++ Sources/SwishCore/BuiltIns.swift
@@ -0,0 +1,12 @@
+// 2107®©™🛀🛀🛀🛀🛀🛀
+
+import Foundation
+
+public final class Builtins {
+    let description = "Builtin functions"
+
+    static func tryme(args:[String]) -> Int! {
+        print("Hello There! I am a built in test for swish!")
+        return 0;
+    }
+}
--- Sources/SwishCore/SwishCore.swift
+++ Sources/SwishCore/SwishCore.swift
@@ -4,16 +4,16 @@ import Foundation
 
 #if os(Linux)
     // https://developer.apple.com/documentation/foundation/nstask
-    typealias Task = Foundation.Task
     import Glibc
 #else
     // https://developer.apple.com/documentation/foundation/process
-    typealias Task = Foundation.Process
     import Darwin.C
 #endif
 
 public final class Core {
     let description = "Swish Core"
+
+    var builtins:Dictionary = ["tryme": Builtins.tryme]
 
     public init() { }
 
@@ -32,19 +32,30 @@ public final class Core {
             let tokens = line.components(separatedBy: separators).filter { !$0.isEmpty }
             guard tokens.count > 0 else { continue }
 
-            var pid: pid_t = 0
-
-            // Convert tokens to C Strings
-            let argv: [UnsafeMutablePointer<CChar>?] = tokens.map{ $0.withCString(strdup)}
-            var status:Int32 = posix_spawnp(&pid, argv[0], nil, nil, argv + [nil], nil)
-
-            // Essentially fork wait
-            if pid < 0 {
-                print ("Error spawning")
-                return;
+            if builtins[tokens[0]] != nil {
+                (builtins[tokens[0]])!(Array(tokens[1...]))
             } else {
-                if waitpid(pid, &status, 0) == -1 {
-                    print("Exited with error")
+                var pid: pid_t = 0
+
+                // Convert tokens to C Strings
+                let argv: [UnsafeMutablePointer<CChar>?] = tokens.map { $0.withCString(strdup)}
+                var status: Int32 = posix_spawnp(&pid, argv[0], nil, nil, argv + [nil], nil)
+
+                //(builtins["tryme"])!(["test"])
+                // Essentially fork wait
+                if pid < 0 {
+                    print ("Error spawning")
+                    return;
+                } else {
+                    // Clear memory for args
+                    argv.forEach {
+                        if let pointer = $0 {
+                            free(pointer)
+                        }
+                    }
+                    if waitpid(pid, &status, 0) == -1 {
+                        print("Exited with error")
+                    }
                 }
             }
         } while(!shouldExit)
