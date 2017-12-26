2270627c59005ad0d85b826ae05aa16655939eb4
rauhul <rauhul.varma@gmail.com>
2017-12-26 01:06:13 -0800

    prettier semi customizable prompt, launch processes with enviornment variables

--- Package.swift
+++ Package.swift
@@ -5,10 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "Swish",
+
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+            url: "https://github.com/onevcat/Rainbow",
+            from: "3.0.0"),
     ],
+
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
@@ -17,7 +22,7 @@ let package = Package(
             dependencies: ["SwishCore"]),
         .target(
             name: "SwishCore",
-            dependencies: []),
+            dependencies: ["Rainbow"]),
         .target(
             name: "ArgumentParser",
             dependencies: []),
--- Sources/swish/main.swift
+++ Sources/swish/main.swift
@@ -1,11 +1,20 @@
+#if os(Linux)
+    import Glibc
+#else
+    import Darwin.C
+#endif
+
 import SwishCore
 
-print("Hello, Swish!")
 
 let core = SwishCore()
 
+signal(SIGINT) { _ in core.handleSIGINT() }
+
+print("Hello, Swish!")
 do {
     try core.run()
 } catch {
     print("error")
 }
+print("Goodbye, Swish!")
