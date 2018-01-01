64aada16d9e41d2a7b7a4d7e9769dae384baf16d
Tanishq Dubey <tanishq.dubey@gmail.com>
2017-12-31 19:07:06 -0600

    All the main functionaluty is done, just some extra stuff

--- Sources/SwishCore/ConsoleIO.swift
+++ Sources/SwishCore/ConsoleIO.swift
@@ -120,18 +120,168 @@ internal final class ConsoleIO {
     }
 
     // MARK:
-    internal func readCommandIn(_ core: SwishCore) -> (command: String, arguments: [CommandArgument])? {
+    internal func readCommand(in core: SwishCore) -> (command: String, arguments: [CommandArgument])? {
         // https://developer.apple.com/documentation/swift/1641199-readline
-        guard let line = readLine(strippingNewline: true) else {
+        guard var line = readLine(strippingNewline: true) else {
             core.shouldExit = true
             return nil
         }
+
 
         let separators = CharacterSet(charactersIn: " \t\r\n")
         let tokens = line.components(separatedBy: separators).filter { !$0.isEmpty }
         guard tokens.count > 0 else { return nil }
 
+        expand(line: tokens[1...].joined(separator: " "))
+
         return (command: tokens[0], arguments: Array(tokens[1...]))
     }
 
+    private func expand(line: String) -> [String] {
+        // 1. Brace Expansion
+        print(braceExpand(line: line).joined(separator: " "))
+        // 2. Tilde Expansion
+        // 3. Shell parameter and variable Expansion
+        // 4. Command substitution
+        // 5. Arithmetic Expansion
+        // 6. Process substitution
+        // 7. Word Splitting
+        // 8. File name expansion
+
+        // {,{,gotta have{ ,\, again\, }}more }cowbell!
+        return []
+    }
+
+    private func braceExpand(line: String) -> [String] {
+        // Wrap entire thing in braces, replace spaces with commas
+        let wrappedLine = "{" + line.replacingOccurrences(of: " ", with: ",") + "}"
+        // The number of brackets and iterator marks
+        let numBrackets = wrappedLine.components(separatedBy: "{").count - 1
+        let numIterator = wrappedLine.components(separatedBy: "..")
+
+        // Just a regular string, return it
+
+        if numBrackets < 1 {
+            print("here1")
+            return [wrappedLine]
+        }
+        let bIndex = wrappedLine.distance(from: wrappedLine.startIndex, to: wrappedLine.index(of: "{")!)
+        let oBIndex = indexOfCharacterAfterFirstClosingBracket(in: wrappedLine)
+
+        if (numBrackets < 2) && ((numIterator.count - 1) > 0) && (bIndex < 1){
+            //TODO: Implement this case
+            print("here2")
+        }
+
+        if (bIndex > 0) || (oBIndex < wrappedLine.count) {
+            print("here3")
+            let s1 = wrappedLine.index(wrappedLine.startIndex, offsetBy: bIndex)
+            let e1 = wrappedLine.index(wrappedLine.endIndex, offsetBy: -1 * oBIndex)
+            return buildCombinations(a: braceExpand(line: String(wrappedLine.prefix(bIndex))), b: buildCombinations(a: braceExpand(line: String(wrappedLine[s1..<e1])), b: braceExpand(line: String(wrappedLine.suffix(oBIndex)))))
+        }
+
+        print("here4")
+        let idx = wrappedLine.index(wrappedLine.startIndex, offsetBy: 1)
+        let idy = wrappedLine.index(wrappedLine.endIndex, offsetBy: -1)
+
+        //TODO: map here
+        //let x = splitElements(line: String(wrappedLine[idx..<idy])).map { braceExpand(line: $0) }
+        //print(x)
+        print(findRange(start: "1", end: "10", step: 1))
+        print(findRange(start: "a", end: "7", step: 1))
+        print(findRange(start: "1", end: "10", step: Int("2")!))
+        print(findRange(start: "a", end: "g", step: Int("3")!))
+        print(findRange(start: "A", end: "Z", step: Int("5")!))
+        return splitElements(line: String(wrappedLine[idx..<idy]))
+    }
+
+    private func indexOfCharacterAfterFirstClosingBracket(in line: String) -> Int {
+        var i = 0
+        var s = 0
+        for idx in line.indices {
+            switch line[idx] {
+            case "{": s += 1
+            case "}": s -= 1
+            default:  break
+            }
+            i = i + 1
+            if s == 0 {
+                return i
+            }
+        }
+        return i
+    }
+
+    private func buildCombinations(a: [String], b: [String]) -> [String] {
+        var ret = [String]()
+        for idx in a {
+            for idy in b {
+                ret.append(idx + idy)
+            }
+        }
+        if ret.count > 0 {
+            return ret
+        } else if a.count > 0 {
+            return a
+        } else {
+            return b
+        }
+    }
+
+    private func splitElements(line: String) -> [String] {
+        if line.count < 1 {
+            return [String]()
+        }
+        if line.last! == "," {
+            return splitElements(line: String(line.prefix(line.count - 1))) + [""]
+        }
+
+        var i = 0
+        var idx = line.index(line.startIndex, offsetBy: i)
+
+        while (i < line.count) && (line[idx] != ",") {
+            i = i + indexOfCharacterAfterFirstClosingBracket(in: String(line.suffix(line.count - i)))
+            idx = line.index(line.startIndex, offsetBy: i)
+        }
+
+        var idz = line.count - (i + 1)
+        if idz < 0 {
+            idz = 0
+        }
+
+        return [String(line.prefix(i))] + splitElements(line: String(line.suffix(idz)))
+    }
+
+    /**
+     * Takes in a start and end of either numbers or characters, and a startIndex
+     * Example: Start 1, End 5, Stride 0 -> 1, 2, 3, 4, 5
+     *          Start a, End e, Stride 2 -> a, c, e
+     */
+    private func findRange(start: String, end: String, step: Int) -> [String] {
+        var step = step
+        var startIdx: Int
+        var endIdx: Int
+
+        if let s = Int(start), let e = Int(end) {
+            startIdx = s
+            endIdx = e
+        } else {
+            startIdx = Int(start.unicodeScalars.map { $0.value }.reduce(0, +))
+            endIdx = Int(end.unicodeScalars.map { $0.value }.reduce(0, +))
+        }
+
+        if endIdx < startIdx {
+            step = -step
+        }
+
+        var ret = [String]()
+        for x in stride(from: startIdx, through: endIdx, by: step) {
+            if !f {
+                ret.append(String(Unicode.Scalar(x)!))
+            } else {
+                ret.append(String(x))
+            }
+        }
+        return ret
+    }
 }
